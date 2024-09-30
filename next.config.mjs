19baf964a90d6f8d822b8d8c720c98bf03e53bba
usama-rahman <rahmanusama390@gmail.com>
2024-09-30 17:45:46 +0600

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "avatar.vercel.sh", port: "", protocol: "https" },
+      { hostname: "utfs.io", port: "", protocol: "https" },
+      {
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
